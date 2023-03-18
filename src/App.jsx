9f8f1199bd8f66d65e71151f3940b56084b710a0
Castillo-Lucas <castillolucas182@gmail.com>
2023-03-18 17:30:50 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <p>Empezando aTempo, el mejor eCommerce que voy a hacer. Hasta el proximo. jaja</p>
+        <p>y vamos probando</p>
+
+    </div>
+  )
+}
+
+export default App
