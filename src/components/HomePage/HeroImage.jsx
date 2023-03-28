c9026543f1525b6095fe9497ac8999c0dbf936b3
Lucas Castillo <103682662+Castillo-Lucas@users.noreply.github.com>
2023-03-28 11:45:56 -0300

    Correcting Error in Hero Image

--- src/components/HomePage/HeroImage.jsx
+++ src/components/HomePage/HeroImage.jsx
@@ -17,144 +17,150 @@ const HeroImage = () => {
   return (
     <div>
       {/*Mobile*/}
-      <Swiper
-        spaceBetween={30}
-        effect={"fade"}
-        loop={true}
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        keyboard={{
-          enabled: true,
-        }}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        modules={[EffectFade, Navigation, Pagination, Keyboard, Autoplay]}
-        className="mySwiper block md:hidden w-full"
-      >
-        <SwiperSlide className="block md:hidden w-96">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/1_qwystt.png"
-                alt=""
-                className="w-full"
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+      <div className="block md:hidden w-full">
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          loop={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          keyboard={{
+            enabled: true,
+          }}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectFade, Navigation, Pagination, Keyboard, Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="block md:hidden w-96">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/1_qwystt.png"
+                  alt=""
+                  className="w-full"
+                />
+              </a>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="block md:hidden">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/2_hmfgsr.png"
-                alt=""
-                className="w-full"
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="block md:hidden">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/2_hmfgsr.png"
+                  alt=""
+                  className="w-full"
+                />
+              </a>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="block md:hidden">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/3_fkpldp.png"
-                alt=""
-                className="w-full"
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="block md:hidden">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/3_fkpldp.png"
+                  alt=""
+                  className="w-full"
+                />
+              </a>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="block prueba md:hidden">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/4_bzwqj3.png"
-                alt=""
-                className="w-full"
-              />
-            </a>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      
+          <SwiperSlide className="block prueba md:hidden">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679610421/aTempo/Assets/homepage/Mobile/4_bzwqj3.png"
+                  alt=""
+                  className="w-full"
+                />
+              </a>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+
+
       {/*Desktop*/}
-      <Swiper
-        spaceBetween={30}
-        effect={"fade"}
-        loop={true}
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        keyboard={{
-          enabled: true,
-        }}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        modules={[EffectFade, Navigation, Pagination, Keyboard, Autoplay]}
-        className="mySwiper hidden md:block"
-      >
-        <SwiperSlide className="hidden md:block">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner1_hfsgvo.png"
-                alt=""
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+      <div className="hidden md:block">
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          loop={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          keyboard={{
+            enabled: true,
+          }}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectFade, Navigation, Pagination, Keyboard, Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="hidden md:block">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner1_hfsgvo.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="hidden md:block">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner2_akcxu4.png"
-                alt=""
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="hidden md:block">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner2_akcxu4.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="hidden md:block">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner3_wcijkg.png"
-                alt=""
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="hidden md:block">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner3_wcijkg.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide className="hidden md:block">
-          {" "}
-          <div>
-            <a href="">
-              <img
-                src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner4_zn8vdt.png"
-                alt=""
-              />
-            </a>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+          <SwiperSlide className="hidden md:block">
+            {" "}
+            <div>
+              <a href="">
+                <img
+                  src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679596627/aTempo/Assets/homepage/banner4_zn8vdt.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+
     </div>
   );
 };
