c88445ea897d1844a53fc3ea831528612c5d8287
Castillo-Lucas <castillolucas182@gmail.com>
2023-03-29 18:05:39 -0300

    Adding Product Cards in Product Page

---
+++ src/components/ProductPage/Card.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="col-span-6 md:col-span-3 relative">
+
+      <div className="absolute border px-3 py-1 mt-4 bg-zinc-700 rounded-r-xl">
+        <p className="text-sm font-medium text-zinc-100">NUEVO</p>
+      </div>
+
+      <div className="bg-transparent border rounded-lg pb-2 lg:pb-0 pt-2 lg:pt-4 cards">
+        <img
+          className="rounded-t-lg h-36 lg:h-40 mx-auto my-auto"
+          src="https://res.cloudinary.com/dthpuldpm/image/upload/v1679417937/aTempo/Assets/Bateria/ddrum%20D2%205-Piece%20Complete%20Drum%20Kit%20Deep%20Aqua%20Sparkle/ddrumAqua1_vrb9b8.png"
+          alt=""
+        />
+
+        <div className="p-1 md:p-2 lg:p-5">
+          <h5 className="text-zinc-800 text-center font-semibold text-lg">
+            Ludwig
+          </h5>
+
+          <div className="h-10 overflow-hidden px-1 mb-4">
+            <p className="text-zinc-500 text-left font-semibold text-sm pb-2">
+              Ludwig BackBeat Complete 5-Piece Drum Set With Hardware and
+              Cymbals Wine Red Sparkle
+            </p>
+          </div>
+
+          <p className="text-zinc-800 text-start font-bold text-base px-1">
+            $<span>1500</span>
+          </p>
+
+          <p className="text-green-600 text-start font-bold text-xs px-1 mb-3">
+            ENVIO GRATIS
+          </p>
+
+          <div className="flex justify-center">
+            <button
+              type="button"
+              className="buttonCard py-1 md:py-2.5 px-1  md:px-5 w-9/12 md:w-full text-sm font-medium text-zinc-800 rounded-md border border-gray-200"
+            >
+              Ver producto
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import BannerFive from "./BannerFive";
+import Card from "./Card";
 import FilterSection from "./FilterSection";
+import "../../App.css";
+import Pagination from "./Pagination";
 
 const ProductPage = () => {
   return (
     <div>
       <BannerFive />
 
-      <div className="lg:container mx-auto 2xl:px-20  grid grid-cols-12 my-4">
+      <div className="lg:container mx-auto 2xl:px-20  grid grid-cols-12 mt-4 mb-3">
+        <FilterSection />
 
-        <FilterSection/>
+        <div className="col-span-12 lg:col-span-10">
+          <div className="grid grid-cols-12 gap-4 md:gap-6 px-4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
 
-        <div className="col col-span-9 bg-red-300/50"></div>
+          <Pagination />
+        </div>
       </div>
     </div>
   );
