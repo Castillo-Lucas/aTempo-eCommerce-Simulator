5a31f1e360693d16cd47266675d0c7b2c124049e
Castillo-Lucas <castillolucas182@gmail.com>
2023-05-09 20:59:29 -0300

    Starting to add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+#aTempo eCommerce Simulator
+
+###This project is an e-commerce simulator focused on musical instruments sale. The application interacts with the user from start to finish, from the homepage to the purchase confirmation page. Products are consumed from Firebase and can be searched and filtered by brand, category, price, and more. A shopping cart is available where you can add and remove products. In the Checkout there are forms where the user must complete them in order to finalize the purchase. The project is fully responsive, allowing interaction with phones, tablets, and computers.
+
+###You can clone this repository with the next command
+
+```
+git clone https://github.com/Castillo-Lucas/aTempo-eCommerce-Simulator.git
+```
--- src/components/OrderConfirmation/OrderConfirmation.jsx
+++ src/components/OrderConfirmation/OrderConfirmation.jsx
@@ -33,20 +33,22 @@ const OrderConfirmation = ({ setShowNavBar }) => {
   useEffect(() => {
     setShowNavBar(true);
 
-    if (
-      paymentInfo[0].paymentType === "creditCard" ||
-      paymentInfo[0].paymentType === "debitCard"
-    ) {
-      //Setting Installments to look like "opcionesDeFormato" format
-      const totCta = Number(totalPurchase) / paymentInfo[0].financing;
-      const totCtaFormat = totCta.toLocaleString("es-ES", opcionesDeFormato);
+    if (orderConfirmation.length !== 0) {
+      if (
+        paymentInfo[0].paymentType === "creditCard" ||
+        paymentInfo[0].paymentType === "debitCard"
+      ) {
+        //Setting Installments to look like "opcionesDeFormato" format
+        const totCta = Number(totalPurchase) / paymentInfo[0].financing;
+        const totCtaFormat = totCta.toLocaleString("es-ES", opcionesDeFormato);
 
-      //Getting the last 4 credit card numbers
-      const numeroTarjeta = paymentInfo[0].cardNumber;
-      const ultimosCuatroDigitos = numeroTarjeta.slice(-4);
+        //Getting the last 4 credit card numbers
+        const numeroTarjeta = paymentInfo[0].cardNumber;
+        const ultimosCuatroDigitos = numeroTarjeta.slice(-4);
 
-      settotCtaFormat(totCtaFormat);
-      setultimosCuatroDigitos(ultimosCuatroDigitos);
+        settotCtaFormat(totCtaFormat);
+        setultimosCuatroDigitos(ultimosCuatroDigitos);
+      }
     }
 
     /*Spinner*/
@@ -74,173 +76,198 @@ const OrderConfirmation = ({ setShowNavBar }) => {
 
   return (
     <div>
-      {spinner ? (
-        <div className="h-screen">
-          <Spinner />
-        </div>
-      ) : (
-        <div className="bodyCart container mx-auto px-2 xl:px-44 py-6">
-          {/*Title*/}
-          <div className="flex flex-col items-center">
-            {" "}
-            <svg
-              width="50px"
-              height="50px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mb-4"
-            >
-              <path
-                opacity="0.15"
-                d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                fill="#F9FAFB"
-              />
-              <path
-                d="M17.0001 9L10 16L7 13M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                stroke="#92D050"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <h2 className="mb-2 text-xl text-zinc-700 font-medium">
-              ¡Gracias por la compra!
-            </h2>
-            <p className="text-zinc-500 text-center">
-              En unos minutos vas a recibir un correo electrónico en{" "}
-              <span className="text-zinc-900 font-medium">
-                {identificationInfo[0].email}
-              </span>{" "}
-              con todos los detalles de tu compra.
-            </p>
-            <p className="text-zinc-500 text-center">
-              Recorda de revisar la carpeta de correo no deseado o spam.
-            </p>
-          </div>
-
-          <div className="grid grid-cols-12 mt-8">
-            {/*Purchase Detail*/}
-            <div className="col-span-12 md:col-span-6 p-4  border-r-0 md:border-r border-zinc-300/70">
-              <p className="text-xl text-zinc-900 font-medium">
-                Orden #{orderNumber}
-              </p>
-              <div className="text-zinc-600 mb-4">
-                {" "}
-                <p>
-                  Compra realizada el {currentDateTime[0].day}/
-                  {currentDateTime[0].month}/{currentDateTime[0].year} a las{" "}
-                  {""}
-                  {currentDateTime[1].hour}:{currentDateTime[1].minutes}
-                </p>
-                <p>Venido y entregado por aTempo</p>
-                <p className="text-zinc-700 font-medium">
-                  {identificationInfo[0].name} {identificationInfo[0].lastName}
-                </p>
-                <p>{identificationInfo[0].email}</p>
-                <p>{identificationInfo[0].DNI}</p>
-                <p>
-                  +54 ({identificationInfo[0].areaNumber}){" "}
-                  {identificationInfo[0].phoneNumber}
-                </p>
-              </div>
-
-              <div className="text-zinc-600 mb-4">
-                <h2 className="text-zinc-900 font-medium">Pago</h2>
-                <p>
-                  Total: <span>${totalPurchaseFormat}</span>
-                </p>
-                <p>
-                  {paymentInfo[0].paymentType === "creditCard"
-                    ? "Tarjeta de Crédito"
-                    : paymentInfo[0].paymentType === "debitCard"
-                    ? "Tarjeta de Débito"
-                    : "Pago en efectivo"}
-                </p>
-                {paymentInfo[0].paymentType === "creditCard" ||
-                paymentInfo[0].paymentType === "debitCard" ? (
-                  <p>
-                    xxxx - xxxx - xxxx - <span>{ultimosCuatroDigitos}</span>
-                  </p>
-                ) : null}
-                {paymentInfo[0].paymentType === "creditCard" ? (
-                  <p>
-                    Financiado en <span>{paymentInfo[0].financing}</span> cuotas
-                    de $<span>{totCtaFormat}</span>
-                  </p>
-                ) : paymentInfo[0].paymentType === "debitCard" ? (
-                  <p>
-                    Se debitará de tu cuenta un pago de $
-                    <span>{totalPurchaseFormat}</span>
-                  </p>
-                ) : (
-                  <p className="">
-                    Deberas abonar por Rapipago o Pago Fácil el valor total de $
-                    <span>{totalPurchaseFormat}</span>
-                  </p>
-                )}
-              </div>
-
-              <div className="text-zinc-600 mb-4">
-                {shippingInfo[0].deliveryMethod === "Store" ? (
-                  <div>
-                    <h2 className="text-zinc-900 font-medium">
-                      Retiro en tienda
-                    </h2>
-                    <p className="text-zinc-400 text-sm">
-                      Listo en las próximas 48hs hábiles
-                    </p>
-                    <p className="text-zinc-600">
-                      Te enviaremos un mail para notificarte cuando tu pedido
-                      este listo para ser retirado
-                    </p>
-                  </div>
-                ) : (
-                  <div>
-                    {" "}
-                    <h2 className="text-zinc-900 font-medium">Envío</h2>
-                    <p className="text-zinc-400 text-sm">
-                      Entrega en 48hs hábiles
-                    </p>
-                    <p>
-                      {shippingInfo[0].street} {shippingInfo[0].streetNumber}
-                      {shippingInfo[0].floor !== "" && (
-                        <span className="text-zinc-600">
-                          , {shippingInfo[0].floor}
-                        </span>
-                      )}
-                    </p>
-                    <p>
-                      {shippingInfo[0].city}, {shippingInfo[0].province}
-                    </p>
-                  </div>
-                )}
-              </div>
+      {orderConfirmation.length !== 0 ? (
+        <div>
+          {" "}
+          {spinner ? (
+            <div className="h-screen">
+              <Spinner />
             </div>
-
-            {/*Product Detail*/}
-            <div className="col-span-12 md:col-span-6 px-4">
-              {/*Product Detail*/}
-              <div className="relative overflow-x-auto max-h-96 border-y md:border-y-0 border-zinc-300/70">
-                {cart.map((currentProduct) => (
-                  <ProductOrder
-                    key={generarID()}
-                    currentProduct={currentProduct}
+          ) : (
+            <div className="bodyCart container mx-auto px-2 xl:px-44 py-6">
+              {/*Title*/}
+              <div className="flex flex-col items-center">
+                {" "}
+                <svg
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mb-4"
+                >
+                  <path
+                    opacity="0.15"
+                    d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                    fill="#F9FAFB"
                   />
-                ))}
+                  <path
+                    d="M17.0001 9L10 16L7 13M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                    stroke="#92D050"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <h2 className="mb-2 text-xl text-zinc-700 font-medium">
+                  ¡Gracias por la compra!
+                </h2>
+                <p className="text-zinc-500 text-center">
+                  En unos minutos vas a recibir un correo electrónico en{" "}
+                  <span className="text-zinc-900 font-medium">
+                    {identificationInfo[0].email}
+                  </span>{" "}
+                  con todos los detalles de tu compra.
+                </p>
+                <p className="text-zinc-500 text-center">
+                  Recorda de revisar la carpeta de correo no deseado o spam.
+                </p>
               </div>
-              {/*Buttons*/}
-              <div className="w-full flex flex-col items-center mt- pt-4">
-                <div className="w-60 flex justify-center">
-                  <NavLink
-                    to="/"
-                    type="button"
-                    className="btnFinCompr py-1 md:py-2.5 px-1  md:px-5 w-9/12 md:w-full text-sm font-medium text-zinc-800 rounded-md border-2 border-gray-200"
-                  >
-                    <p className="text-center">Seguir comprando</p>
-                  </NavLink>
+
+              <div className="grid grid-cols-12 mt-8">
+                {/*Purchase Detail*/}
+                <div className="col-span-12 md:col-span-6 p-4  border-r-0 md:border-r border-zinc-300/70">
+                  <p className="text-xl text-zinc-900 font-medium">
+                    Orden #{orderNumber}
+                  </p>
+                  <div className="text-zinc-600 mb-4">
+                    {" "}
+                    <p>
+                      Compra realizada el {currentDateTime[0].day}/
+                      {currentDateTime[0].month}/{currentDateTime[0].year} a las{" "}
+                      {""}
+                      {currentDateTime[1].hour}:{currentDateTime[1].minutes}
+                    </p>
+                    <p>Venido y entregado por aTempo</p>
+                    <p className="text-zinc-700 font-medium">
+                      {identificationInfo[0].name}{" "}
+                      {identificationInfo[0].lastName}
+                    </p>
+                    <p>{identificationInfo[0].email}</p>
+                    <p>{identificationInfo[0].DNI}</p>
+                    <p>
+                      +54 ({identificationInfo[0].areaNumber}){" "}
+                      {identificationInfo[0].phoneNumber}
+                    </p>
+                  </div>
+
+                  <div className="text-zinc-600 mb-4">
+                    <h2 className="text-zinc-900 font-medium">Pago</h2>
+                    <p>
+                      Total: <span>${totalPurchaseFormat}</span>
+                    </p>
+                    <p>
+                      {paymentInfo[0].paymentType === "creditCard"
+                        ? "Tarjeta de Crédito"
+                        : paymentInfo[0].paymentType === "debitCard"
+                        ? "Tarjeta de Débito"
+                        : "Pago en efectivo"}
+                    </p>
+                    {paymentInfo[0].paymentType === "creditCard" ||
+                    paymentInfo[0].paymentType === "debitCard" ? (
+                      <p>
+                        xxxx - xxxx - xxxx - <span>{ultimosCuatroDigitos}</span>
+                      </p>
+                    ) : null}
+                    {paymentInfo[0].paymentType === "creditCard" ? (
+                      <p>
+                        Financiado en <span>{paymentInfo[0].financing}</span>{" "}
+                        cuotas de $<span>{totCtaFormat}</span>
+                      </p>
+                    ) : paymentInfo[0].paymentType === "debitCard" ? (
+                      <p>
+                        Se debitará de tu cuenta un pago de $
+                        <span>{totalPurchaseFormat}</span>
+                      </p>
+                    ) : (
+                      <p className="">
+                        Deberas abonar por Rapipago o Pago Fácil el valor total
+                        de $<span>{totalPurchaseFormat}</span>
+                      </p>
+                    )}
+                  </div>
+
+                  <div className="text-zinc-600 mb-4">
+                    {shippingInfo[0].deliveryMethod === "Store" ? (
+                      <div>
+                        <h2 className="text-zinc-900 font-medium">
+                          Retiro en tienda
+                        </h2>
+                        <p className="text-zinc-400 text-sm">
+                          Listo en las próximas 48hs hábiles
+                        </p>
+                        <p className="text-zinc-600">
+                          Te enviaremos un mail para notificarte cuando tu
+                          pedido este listo para ser retirado
+                        </p>
+                      </div>
+                    ) : (
+                      <div>
+                        {" "}
+                        <h2 className="text-zinc-900 font-medium">Envío</h2>
+                        <p className="text-zinc-400 text-sm">
+                          Entrega en 48hs hábiles
+                        </p>
+                        <p>
+                          {shippingInfo[0].street}{" "}
+                          {shippingInfo[0].streetNumber}
+                          {shippingInfo[0].floor !== "" && (
+                            <span className="text-zinc-600">
+                              , {shippingInfo[0].floor}
+                            </span>
+                          )}
+                        </p>
+                        <p>
+                          {shippingInfo[0].city}, {shippingInfo[0].province}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                {/*Product Detail*/}
+                <div className="col-span-12 md:col-span-6 px-4">
+                  {/*Product Detail*/}
+                  <div className="relative overflow-x-auto max-h-96 border-y md:border-y-0 border-zinc-300/70">
+                    {cart.map((currentProduct) => (
+                      <ProductOrder
+                        key={generarID()}
+                        currentProduct={currentProduct}
+                      />
+                    ))}
+                  </div>
+                  {/*Buttons*/}
+                  <div className="w-full flex flex-col items-center mt- pt-4">
+                    <div className="w-60 flex justify-center">
+                      <NavLink
+                        to="/"
+                        type="button"
+                        className="btnFinCompr py-1 md:py-2.5 px-1  md:px-5 w-9/12 md:w-full text-sm font-medium text-zinc-800 rounded-md border-2 border-gray-200"
+                      >
+                        <p className="text-center">Seguir comprando</p>
+                      </NavLink>
+                    </div>
+                  </div>
                 </div>
               </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <div className="container mx-auto mb-10 notFound">
+            <img
+              src="https://res.cloudinary.com/dthpuldpm/image/upload/v1680867172/aTempo/error_q7wium.jpg"
+              alt=""
+            />
+
+            <div className="flex justify-center">
+              <NavLink
+                to="/"
+                className="buttonCard text-center py-1 md:py-2.5 px-1  md:px-5 w-24 text-sm font-medium text-zinc-800 rounded-md border border-zinc-300/80"
+              >
+                Volver!
+              </NavLink>
             </div>
           </div>
         </div>
